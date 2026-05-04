6b76e0de1c7a50b75cae5d5e99ac5d7249dd9a22
freddycapo <federicocaposano@gmail.com>
2026-05-04 14:43:29 +0200

    fix : changed formModule to ReactiveFormModule

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule , FormBuilder , Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import {
   IonHeader, IonToolbar, IonTitle, IonContent,
   IonList, IonItem, IonInput, IonButton, IonText,
 } from '@ionic/angular/standalone';
+import { AuthService } from 'src/app/services/auth';
 
 @Component({
   selector: 'app-login',
@@ -12,20 +13,49 @@ import {
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   imports: [
-    FormsModule, RouterLink,
+    RouterLink,
+    ReactiveFormsModule,
     IonHeader, IonToolbar, IonTitle, IonContent,
-    IonList, IonItem, IonInput, IonButton, IonText,
+    IonList, IonItem, IonInput, IonButton, IonText
   ],
 })
 export class LoginPage {
   email = '';
   password = '';
 
-  constructor(private router: Router) {}
+  private authService = inject(AuthService);
+  private formBuilder = inject(FormBuilder);
+  private router = inject(Router);
 
-  onLogin() {
-    // TODO: replace with real auth call
-    console.log('Login attempt:', this.email);
-    this.router.navigateByUrl('/home');
+  loginForm = this.formBuilder.nonNullable.group(
+    {
+      email: ['' , Validators.required , Validators.minLength(1)],
+      password: ['' , Validators.required , Validators.minLength(1)]
+    }
+  )
+
+  submitting = false;
+  errorMessage = '';
+
+  async onLogin() {
+    if (this.loginForm.invalid) return;
+
+    this.submitting = true;
+    this.errorMessage = '';
+
+    const { email, password } = this.loginForm.getRawValue();
+    const ok = await this.authService.login(email , password);
+
+    this.submitting = true;
+
+    if (ok){
+      console.log('Login attempt:', this.email);
+      this.router.navigateByUrl('/home')
+    } else{
+      this.submitting = false;
+      this.errorMessage = 'Login fallito come te'
+      console.log("Login Error!");
+    }
+
   }
 }
